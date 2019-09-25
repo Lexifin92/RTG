3edc24fe4bbebab18c2158e581252a718686dc5d
LEXI <s1900573@edu.bc.fi>
2019-09-25 14:44:01 +0300

    Välilehtien luonti ja url linkitykset. ok

---
+++ sites/default/settings.php
@@ -0,0 +1,13 @@
+<?php
+$databases['default']['default'] = array (
+  'database' => 'RoniBack',
+  'username' => 's1900573',
+  'password' => '159753',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+$settings['hash_salt'] = 'Bwc9ochaQfctokAuNtXr_4-jn5zRD7IN3LgedRRk8Ag-VSnNP0_OyjjD7a-n9EbAw_fJZL-VSw';
+$config_directories['sync'] = 'sites/default/files/config_vw4V5lzb8TUyVIBLHOiuGiERy8kdYQSCZAaBm3onKl_HJmQ8RZ2EyYHgcqLGkXyFxQLJB-fxVA/sync';
